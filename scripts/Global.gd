ced006d32d4f90ec8ba161c14536b9af96717a4b
Thomas-John <123978285+ThomasJVasile@users.noreply.github.com>
2025-04-05 15:58:16 -0700

    done
    doen

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 extends Node
 
 var has_map = false
@@ -10,3 +11,17 @@ func time_penalty(seconds):
 	if main.has_method("apply_time_penalty"):
 		main.apply_time_penalty(seconds)
 var has_ghost_slayer_title = false
+=======
+#extends Node
+#
+#var has_map = false
+#var has_usb = false
+#var has_frog_token = false
+#var has_bad_ui = false
+#
+#func time_penalty(seconds):
+	#var main = get_tree().get_root().get_node("Main")
+	#if main.has_method("apply_time_penalty"):
+		#main.apply_time_penalty(seconds)
+#var has_ghost_slayer_title = false
+>>>>>>> Stashed changes
