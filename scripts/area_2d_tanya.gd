8b82f5674db5c6ae436171b1597668b7c6b92c09
PieFlavr <lucasadampinto@gmail.com>
2025-04-06 00:24:59 -0700

    Commented out _process override that was crashing the game.

--- scripts/area_2d_tanya.gd
+++ scripts/area_2d_tanya.gd
@@ -34,24 +34,24 @@ func _on_body_entered(body: Node2D) -> void:
 		elif response == 1 :
 			conversation_menu.displayText("Bold move. She gives you her Golden Planner!","Tanya")
 			
+#
+#func _process(_delta: float) -> void:
+	#if interacted:
+		#return
 
-func _process(_delta: float) -> void:
-	if interacted:
-		return
-
-	if Input.is_action_just_pressed("ui_accept"):  # Z
-		conversation_menu.set_text("You help run Laser Chess! She gives you a badge.")
-		Global.has_council_badge = true
-		interacted = true
-
-	elif Input.is_action_just_pressed("ui_cancel"):  # X
-		if randf() > 0.5:
-			conversation_menu.set_text("Bold move. She gives you her Golden Planner!")
-			Global.has_time_rewind = true
-		else:
-			conversation_menu.set_text("She rejects you on the campus PA system. Brutal.")
+	#if Input.is_action_just_pressed("ui_accept"):  # Z
+		#conversation_menu.displayText("You help run Laser Chess! She gives you a badge.","Tanya")
+		#Global.has_council_badge = true
+		#interacted = true
+#
+	#elif Input.is_action_just_pressed("ui_cancel"):  # X
+		#if randf() > 0.5:
+			#conversation_menu.displayText("Bold move. She gives you her Golden Planner!","Tanya")
+			#Global.has_time_rewind = true
+		#else:
+			#conversation_menu.displayText("She rejects you on the campus PA system. Brutal.","Tanya")
 			#Global.time_penalty(30)
-		interacted = true
+		#interacted = true
 
 func _on_body_exited(body: Node2D) -> void:
 	conversation_menu.closeMenu()
