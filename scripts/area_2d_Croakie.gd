7d77b33195275201e0995996d993db5769913400
1106-Hays-Malcolm <mah2005atM@gmail.com>
2025-04-05 20:36:07 -0700

    Added Kyle npc and proper texture

--- scripts/area_2d_Croakie.gd
+++ scripts/area_2d_Croakie.gd
@@ -3,9 +3,7 @@ extends Area2D
 @onready var conversation_menu: Control = %ConversationMenu
 
 func _on_body_entered(body: Node2D) -> void:
-	print("Tanya1")
 	if body.name == "Player":
-		print("Tanya2")
 		conversation_menu.displayQuestion(["Two", "Three"])
 		
 
---
+++ scripts/area_2d_tanya.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@onready var conversation_menu: Control = %ConversationMenu
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		conversation_menu.displayQuestion(["Two", "Three"])
+		
+
+
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	conversation_menu.closeMenu()
+	print("Now closing menu")
