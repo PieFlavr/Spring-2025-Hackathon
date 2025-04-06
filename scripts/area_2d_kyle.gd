ce1e3c0cca17491f2323fb6e13049bcf746972bc
1106-Hays-Malcolm <mah2005atM@gmail.com>
2025-04-05 21:34:20 -0700

    Fixed kyle npc

--- scripts/area_2d_kyle.gd
+++ scripts/area_2d_kyle.gd
@@ -1,1 +1,16 @@
 extends Area2D
+
+@onready var conversation_menu: Control = %ConversationMenu
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		conversation_menu.displayQuestion("Test kyle", ["Two", "Three"], "Kyle")
+		
+
+
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	conversation_menu.closeMenu()
+	print("Now closing menu")
