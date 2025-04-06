7569a8e634c3b777d751fef62534e80b9f33ecc8
PieFlavr <lucasadampinto@gmail.com>
2025-04-06 02:45:30 -0700

    Cleaned up Player Scene and fixed transition bugs.

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 @export var dash_cooldown: float = 0.5
 
 # Node Access Variables
-var anim_sprite
+@onready var anim_sprite = $AnimatedSprite2D
 
 # State Variables
 var state = states.idle
@@ -19,20 +19,18 @@ enum states {
 	act,
 } # for managing interaction and dialouge -L
 
-var input_vector = Vector2()
+var input_vector = Vector2.ZERO
 
 # initializing
 var can_dash = true
 var dash_timer = 0.0
 var dash_direction = Vector2.ZERO
 
-func _ready() -> void:
-	anim_sprite = $AnimatedSprite2D
-
 func check_state():
-	prev_state = state
 	# if not acting, do anything else
-	if(state != states.act && state != states.dash):
+	if(state == states.act):
+		update_state(state)
+	elif(state != states.dash):
 		# moved input vector checking to state_check event to avoid _prcoess() nastiness -L
 		input_vector = Vector2(
 			Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
@@ -43,18 +41,22 @@ func check_state():
 			update_state(states.walk)
 		# if walking, and not movement keys, idle -L
 		elif (input_vector == Vector2.ZERO && state == states.walk):
-			state = states.idle
+			update_state(states.idle)
 		# if walking, and pressed dash, then dash -L
 		elif((state == states.walk && Input.is_action_just_pressed("dash") && can_dash)
 			&& (dash_timer <= 0.0)):
-			state = states.dash
+			update_state(states.dash)
+		else:
+			update_state(state)
+	else:
+		update_state(state)
 	
 func _physics_process(delta):
-	
 	check_state()
-	if(prev_state != state):
+	
+	if(prev_state != state): 
 		print("State transition ", prev_state, " to ", state)
-		
+	
 	match(state): #process state
 		states.idle:
 			do_idle_state(prev_state, delta)
@@ -65,25 +67,22 @@ func _physics_process(delta):
 		states.act:
 			do_act_state(prev_state,delta)
 			
-
+#region STATE TRANSITION FUNCTIONs
 func do_idle_state(prev_state: int, delta: float):
-	if(prev_state != states.idle):
-		anim_sprite.play("idle")
+	transition_animation("idle")
 
 func do_walk_state(prev_state: int, delta: float):
-	if(prev_state != states.walk):
-		if abs(input_vector.x) > abs(input_vector.y):
-			anim_sprite.play("walk_right")
-			anim_sprite.flip_h = input_vector.x < 0
-		else:
-			anim_sprite.play("walk_down" if input_vector.y > 0 else "walk_up")
+	transition_animation("walk")
+	if abs(input_vector.x) > abs(input_vector.y):
+		anim_sprite.flip_h = input_vector.x < 0
 	velocity = input_vector * speed 
 	
 	move_and_slide() # process movement
-	
+
 func do_dash_state(prev_state: int, delta: float):
+	transition_animation("dash")
+	
 	if(prev_state != states.dash): #perform transition stuff -L
-		update_state(states.dash)
 		can_dash = false
 	
 	velocity = input_vector * dash_speed
@@ -94,22 +93,23 @@ func do_dash_state(prev_state: int, delta: float):
 		can_dash = true
 		dash_timer = 0
 		update_state(states.walk)
-		
-func do_act_state(prev_state: int, delta: float):
-	if(prev_state != states.act):
-		anim_sprite.play("idle")
-		print("Now Acting!")
 
-"""
-CONVERSATION/ACT LOCK FUNCTION OVERRIDES
-"""
+func do_act_state(prev_state: int, delta: float):
+	transition_animation("idle")
+		
+func transition_animation(animation_name: String):
+	if(anim_sprite.is_playing() && anim_sprite.animation != animation_name):
+		anim_sprite.play(animation_name)
+#endregion
+
+#region CONVERSATION ACT LOCK FUNCTIONs
 func _on_conversation_menu_lock_controls() -> void:
 	update_state(states.act)
-	
+
 func _on_conversation_menu_unlock_controls() -> void:
 	update_state(states.idle)
 
 func update_state(new_state: int):
 	prev_state = state
 	state = new_state
-	
+#endregion
